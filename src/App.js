025a228a76b519459879843c20042e838124ba4d
Prosper Nglazi <prosperinweb@gmail.com>
2021-10-11 18:31:51 +0200

    add linting errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/container/Home/Home';
 import Search from './components/container/Search/Search';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Route exact path="/" component={Home} />
       <Route path="/search" component={Search} />
-    </div>
+    </Router>
   );
 }
 
